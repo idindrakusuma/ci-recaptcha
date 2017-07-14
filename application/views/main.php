8bbe3948e48ad6c45418886cbc0e09211051c6ea
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-07-14 23:52:01 +0700

    first commit

---
+++ application/views/main.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>reCAPTCHA Example</title>
+</head>
+<body>
+<form action="#" method="post">
+<?php echo $widget;?>
+<?php echo $script;?>
+<br />
+<input type="submit" value="submit" />
+</form>
+</body>
+</html>
